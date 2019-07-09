d1a0beb0e34472ea44020bfef52437653bed9337
Tazro Inutano Ohta <inutano@gmail.com>
2019-07-09 20:16:28 +0900

    change test case

--- ftpGet_test.go
+++ ftpGet_test.go
@@ -4,6 +4,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -16,14 +17,17 @@ func TestFtpGet(t *testing.T) {
 	tests := []struct {
 		name, url string
 	}{
-		{"DRR007230 from EBI", "ftp://ftp.sra.ebi.ac.uk/vol1/drr/DRR007/DRR007230"},
+		{"DRR007230 from DDBJ", "ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/sralite/ByExp/litesra/DRX/DRX006/DRX006419/DRR007230/DRR007230.sra"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			u, _ := url.Parse(tt.url)
 			FtpGet(u)
-			fileName := filepath.Base(tt.url) + ".sra"
+			fileName := strings.TrimSuffix(filepath.Base(tt.url), filepath.Ext(tt.url)) + ".sra"
 			if exists(fileName) != true {
+				t.Errorf("Failed FtpGet()")
+			}
+			if err := os.Remove(fileName); err != nil {
 				t.Errorf("Failed FtpGet()")
 			}
 		})
